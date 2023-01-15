08689ef931dda96c634bf8044835466ffd836a3d
Shaun Evening <contactshaunlloyd+github@gmail.com>
2023-01-15 15:34:44 -0500

    Initial commit

---
+++ src/Tool.tsx
@@ -0,0 +1,27 @@
+import React, { useCallback } from "react";
+import { useGlobals } from "@storybook/api";
+import { Icons, IconButton } from "@storybook/components";
+import { TOOL_ID } from "./constants";
+
+export const Tool = () => {
+  const [{ myAddon }, updateGlobals] = useGlobals();
+
+  const toggleMyTool = useCallback(
+    () =>
+      updateGlobals({
+        myAddon: myAddon ? undefined : true,
+      }),
+    [myAddon]
+  );
+
+  return (
+    <IconButton
+      key={TOOL_ID}
+      active={myAddon}
+      title="Enable my addon"
+      onClick={toggleMyTool}
+    >
+      <Icons icon="lightning" />
+    </IconButton>
+  );
+};
