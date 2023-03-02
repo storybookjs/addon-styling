1644c69fd8d915b02da9557aa16528f800be831d
Shaun Evening <shaun@chromatic.com>
2023-03-02 13:26:10 -0500

    feat(sass): Add SASS support to addon (#4)
    * Remove unneeded file
    * Change ADDON_ID
    * Split webpackFinal for css and sass
    * Add optional peerdeps
    * Update bootstrap recipe

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const ADDON_ID = "storybook/addon-theming" as const;
+export const ADDON_ID = "@storybook/addon-styling" as const;
 export const THEME_SWITCHER_ID = `${ADDON_ID}/theme-switcher` as const;
 export const PARAM_KEY = "theming" as const;
 export const GLOBAL_KEY = "theme" as const;
