e3ad85db10773f632a956c610058bcd50ed4ddaf
Shaun Lloyd <shaun@chromatic.com>
2023-05-16 19:15:49 -0400

    Add descriptions for the strategy interface

--- src/postinstall/types.ts
+++ src/postinstall/types.ts
@@ -23,13 +23,33 @@ export type SupportedStylingTools =
   typeof SUPPORTED_STYLING_TOOLS[keyof typeof SUPPORTED_STYLING_TOOLS];
 
 export interface ToolConfigurationStrategy {
+  /**
+   * The name of the tool for configuration
+   */
   name: SupportedStylingTools;
+  /**
+   * Predicate function to check if the project meets the requirements of this strategy
+   * @param packageJson The project's package.json
+   * @returns {boolean} whether the project has the tool
+   */
   predicate: (packageJson: PackageJson) => Boolean;
+  /**
+   * Transform function for a `.storybook/main.ts` file
+   * @param builder The builder used to build the project's stories
+   * @param mainConfig Babel AST for the main.ts file
+   * @param logger logging utilities
+   */
   main?: (
     builder: SupportedBuilders,
     mainConfig: ConfigFile,
     logger: NodeLogger
   ) => void;
+  /**
+   * Transform function for a `.storybook/preview.ts` file
+   * @param builder The builder used to build the project's stories
+   * @param mainConfig Babel AST for the preview.ts file
+   * @param logger logging utilities
+   */
   preview?: (
     builder: SupportedBuilders,
     previewConfig: ConfigFile,
