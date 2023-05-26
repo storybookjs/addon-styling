89c09fa10ee770f5fa7013db1781c5b11933d9a1
Shaun Lloyd <shaun@chromatic.com>
2023-05-26 10:34:57 -0400

    Write pretty printing function

--- src/postinstall/utils/configs.utils.ts
+++ src/postinstall/utils/configs.utils.ts
@@ -1,4 +1,8 @@
 import fs from "node:fs/promises";
+import prettier from 'prettier';
+import * as recast from 'recast';
+
+import { logger } from "@storybook/node-logger";
 import { type ConfigFile, readConfig } from "@storybook/csf-tools";
 
 const SUPPORTED_EXTENSIONS = ["js", "ts", "tsx", "jsx"] as const;
@@ -29,5 +33,30 @@ export const getMainConfig = async (): Promise<ConfigFile> => {
 
   return mainConfig;
 };
+
+export const writePrettyConfig = async (config: ConfigFile): Promise<void> => {
+  const fname = config.fileName;
+  if (!fname) throw new Error('Please specify a fileName for writeConfig');
+
+  let output = recast.print(config._ast, {}).code
+
+  try {
+    const prettierConfig = prettier.resolveConfig.sync('.', { editorconfig: true }) || {
+      printWidth: 100,
+      tabWidth: 2,
+      bracketSpacing: true,
+      trailingComma: 'es5',
+      singleQuote: true,
+    };
+
+    output = prettier.format(output, { ...prettierConfig, filepath: fname });
+  } catch (e) {
+    logger.info(`Failed applying prettier to ${fname}.`);
+  }
+
+  await fs.writeFile(fname, output);
+  
+
+}
 
 
