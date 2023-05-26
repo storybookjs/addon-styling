691818e46cfe4014c52357e81156dd481beac7b9
Shaun Lloyd <shaun@chromatic.com>
2023-05-26 10:33:53 -0400

    Use recast for AST manipulation

--- src/postinstall/utils/babel.utils.ts
+++ src/postinstall/utils/babel.utils.ts
@@ -1,18 +1,12 @@
-import { babelParse } from "@storybook/csf-tools";
+import * as recast from "recast";
 
 import traverse from "@babel/traverse";
 import * as t from "@babel/types";
 
-export const AddCommentBeforeBabelNode = (
-  comment: string,
-  node: any,
-  line: boolean = false
-) => {
-  t.addComment(node, "leading", comment, line);
+export const stringToNode = (code: TemplateStringsArray) => {
+  const result = recast.parse(code.join(""));
+  return result.program.body;
 };
-
-export const parseStringToBabelNode = (code: string) =>
-  babelParse(code).program.body[0];
 
 export const addImports = (ast: t.File, statements: t.Statement[]) => {
   let inserted = false;
