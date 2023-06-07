f3962ed844b38309fbb061eb367972bba7ccc06b
Shaun Lloyd <shaun@chromatic.com>
2023-06-07 09:33:53 -0400

    Add test suite for MUI codemod

---
+++ src/postinstall/material-ui/material-ui.strategy.test.ts
@@ -0,0 +1,208 @@
+import { describe, it, expect, vi } from "vitest";
+import { PackageJson } from "@storybook/types";
+import { readConfig } from "@storybook/csf-tools";
+import { logger, colors } from "@storybook/node-logger";
+import { resolve } from "node:path";
+
+import { materialUIStrategy } from "./material-ui.strategy";
+import { SUPPORTED_BUILDERS } from "../types";
+import { formatFileContents } from "../utils/configs.utils";
+
+vi.importMock("@storybook/node-logger");
+const mockedLogger = { logger, colors };
+
+describe("CODEMOD: Material UI configuration", () => {
+  describe("PREDICATE: should project be configured for Material UI?", () => {
+    it("TRUE: it should return true when Material UI is found in package.json", () => {
+      const packageJson: PackageJson = {
+        dependencies: {
+          "@mui/material": "latest",
+          "@emotion/react": "latest",
+          "@emotion/styled": "latest",
+        },
+        devDependencies: {},
+      };
+
+      const result = materialUIStrategy.predicate(packageJson);
+
+      expect(result).toBeTruthy();
+    });
+    it("FALSE: it should return false when Material UI is not found in package.json", () => {
+      const packageJson: PackageJson = {
+        dependencies: { bootstrap: "latest" },
+        devDependencies: {},
+      };
+
+      const result = materialUIStrategy.predicate(packageJson);
+
+      expect(result).toBeFalsy();
+    });
+  });
+
+  describe("MAIN: how should storybook be configured for Material UI", () => {
+    it("NO-OP: No changes should be made to storybook's main.ts file", async () => {
+      const mainConfig = await readConfig(
+        resolve(__dirname, "../fixtures/main.fixture.ts")
+      );
+      const packageJson: PackageJson = {
+        dependencies: {
+          "@mui/material": "latest",
+          "@emotion/react": "latest",
+          "@emotion/styled": "latest",
+        },
+        devDependencies: { postcss: " latest" },
+      };
+
+      materialUIStrategy.main(
+        mainConfig,
+        packageJson,
+        SUPPORTED_BUILDERS.VITE,
+        mockedLogger
+      );
+
+      const result = formatFileContents(mainConfig);
+
+      expect(result).toMatchInlineSnapshot(`
+        "import type { StorybookConfig } from \\"@storybook/react-vite\\";
+        const config: StorybookConfig = {
+          stories: [\\"../stories/**/*.stories.@(js|jsx|ts|tsx)\\"],
+          addons: [\\"@storybook/addon-essentials\\"],
+          framework: {
+            name: \\"@storybook/react-vite\\",
+            options: {},
+          },
+          docs: {
+            autodocs: true,
+          },
+        };
+        export default config;
+        "
+      `);
+    });
+  });
+
+  describe("PREVIEW: how should storybook preview be configured for Material UI", () => {
+    it("NO FONT: Preview.ts should be updated with the imports, theme decorator, and a todo for importing fonts", async () => {
+      const previewConfig = await readConfig(
+        resolve(__dirname, "../fixtures/preview.fixture.ts")
+      );
+      const packageJson: PackageJson = {
+        dependencies: {
+          "@mui/material": "latest",
+          "@emotion/react": "latest",
+          "@emotion/styled": "latest",
+        },
+        devDependencies: { postcss: " latest" },
+      };
+
+      materialUIStrategy.preview(
+        previewConfig,
+        packageJson,
+        SUPPORTED_BUILDERS.WEBPACK,
+        mockedLogger
+      );
+
+      const result = formatFileContents(previewConfig);
+
+      expect(result).toMatchInlineSnapshot(`
+        "import type { Preview } from \\"@storybook/react\\";
+
+        import { ThemeProvider, CssBaseline } from \\"@mui/material\\";
+        import { withThemeFromJSXProvider } from \\"@storybook/addon-styling\\";
+
+        // TODO: update import for your custom Material UI themes
+        // import { lightTheme, darkTheme } from '../path/to/themes';
+
+        // Import your fontface CSS files here
+        // Don't have any? We recommend installing and using @fontsource/roboto
+
+        const preview: Preview = {
+          parameters: {
+            theming: {},
+          },
+
+          decorators: [
+            // Adds global styles and theme switching support.
+            withThemeFromJSXProvider({
+              GlobalStyles: CssBaseline,
+              // Uncomment for theme switching
+              // Provider: ThemeProvider,
+              // themes: {
+              // Provide your custom themes here
+              //   light: lightTheme,
+              //   dark: darkTheme,
+              // },
+              // defaultTheme: 'light',
+            }),
+          ],
+        };
+
+        export default preview;
+        "
+      `);
+    });
+
+    it("FONT: Preview.ts should be updated with the imports, theme decorator, and roboto fonts", async () => {
+      const previewConfig = await readConfig(
+        resolve(__dirname, "../fixtures/preview.fixture.ts")
+      );
+      const packageJson: PackageJson = {
+        dependencies: {
+          "@mui/material": "latest",
+          "@emotion/react": "latest",
+          "@emotion/styled": "latest",
+          "@fontsource/roboto": "latest",
+        },
+        devDependencies: { postcss: " latest" },
+      };
+
+      materialUIStrategy.preview(
+        previewConfig,
+        packageJson,
+        SUPPORTED_BUILDERS.WEBPACK,
+        mockedLogger
+      );
+
+      const result = formatFileContents(previewConfig);
+
+      expect(result).toMatchInlineSnapshot(`
+        "import type { Preview } from \\"@storybook/react\\";
+
+        import { ThemeProvider, CssBaseline } from \\"@mui/material\\";
+        import { withThemeFromJSXProvider } from \\"@storybook/addon-styling\\";
+
+        // TODO: update import for your custom Material UI themes
+        // import { lightTheme, darkTheme } from '../path/to/themes';
+
+        import \\"@fontsource/roboto/300.css\\";
+        import \\"@fontsource/roboto/400.css\\";
+        import \\"@fontsource/roboto/500.css\\";
+        import \\"@fontsource/roboto/700.css\\";
+
+        const preview: Preview = {
+          parameters: {
+            theming: {},
+          },
+
+          decorators: [
+            // Adds global styles and theme switching support.
+            withThemeFromJSXProvider({
+              GlobalStyles: CssBaseline,
+              // Uncomment for theme switching
+              // Provider: ThemeProvider,
+              // themes: {
+              // Provide your custom themes here
+              //   light: lightTheme,
+              //   dark: darkTheme,
+              // },
+              // defaultTheme: 'light',
+            }),
+          ],
+        };
+
+        export default preview;
+        "
+      `);
+    });
+  });
+});
