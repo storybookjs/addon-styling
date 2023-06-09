d5a07c7c24a3c20214adfce1dbef773885d019c8
Shaun Lloyd <shaun@chromatic.com>
2023-06-09 14:57:04 -0400

    Add auto configs for emotion and styled components

---
+++ src/postinstall/emotion/emotion.strategy.ts
@@ -0,0 +1,101 @@
+import { PackageJson } from "@storybook/types";
+import { logger, colors } from "@storybook/node-logger";
+import * as t from "@babel/types";
+
+import { hasDependency } from "../utils/dependencies.utils";
+import { SUPPORTED_STYLING_TOOLS, ToolConfigurationStrategy } from "../types";
+import { addImports, stringToNode } from "../utils/babel.utils";
+
+const projectHasEmotion = (packageJson: PackageJson) =>
+  hasDependency(packageJson, "@emotion/react") &&
+  hasDependency(packageJson, "@emotion/styled");
+
+export const emotionStrategy: ToolConfigurationStrategy = {
+  name: SUPPORTED_STYLING_TOOLS.EMOTION,
+  predicate: projectHasEmotion,
+  main: (mainConfig, packageJson, builder) => {
+    logger.plain(`  • Registering ${colors.pink("@storybook/addon-styling")}.`);
+
+    const [addonConfigNode] = stringToNode`({
+      name: "@storybook/addon-styling",
+      options: {}
+    })`;
+
+    const addonsNodePath = ["addons"];
+    let addonsArrayNode = mainConfig.getFieldNode(addonsNodePath);
+
+    if (!addonsArrayNode) {
+      mainConfig.setFieldNode(addonsNodePath, t.arrayExpression());
+      addonsArrayNode = mainConfig.getFieldNode(addonsNodePath);
+    }
+
+    // @ts-expect-error
+    addonsArrayNode.elements.push(addonConfigNode);
+  },
+  preview: (previewConfig, packageJson, builder) => {
+    logger.plain(
+      `  • Adding imports for ${colors.green(
+        SUPPORTED_STYLING_TOOLS.EMOTION
+      )}, ${colors.blue("ThemeProvider")}, ${colors.blue(
+        "Global"
+      )}, and ${colors.blue("css")}`
+    );
+    logger.plain(
+      `  • Adding import for ${colors.blue(
+        "withThemeFromJSXProvider"
+      )} decorator`
+    );
+
+    const importsNode = stringToNode`
+    import { Global, css, ThemeProvider } from '@emotion/react';
+    import { withThemeFromJSXProvider } from '@storybook/addon-styling';
+
+    /* TODO: update import for your custom theme configurations */
+    // import { lightTheme, darkTheme } from '../path/to/themes';
+    
+    /* TODO: replace with your own global styles, or remove */
+    const GlobalStyles = () => (
+        <Global
+          styles={css\`
+            body {
+              font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+            }
+          \`}
+        />
+      );
+    `;
+
+    addImports(previewConfig._ast, importsNode);
+
+    logger.plain(
+      `  • Adding ${colors.blue(
+        "withThemeFromJSXProvider"
+      )} decorator to config`
+    );
+    const [
+      decoratorNode,
+    ] = stringToNode`// Adds global styles and theme switching support.
+    withThemeFromJSXProvider({
+        /* Uncomment for theme switching support */
+        // themes: {
+        //   light: lightTheme,
+        //   dark: darkTheme,
+        // }
+        // defaultTheme: 'light',
+        // Provider: ThemeProvider,
+        GlobalStyles,
+      })`;
+
+    const decoratorNodePath = ["decorators"];
+    let decoratorArrayNode = previewConfig.getFieldNode(decoratorNodePath);
+
+    if (!decoratorArrayNode) {
+      previewConfig.setFieldNode(decoratorNodePath, t.arrayExpression());
+      decoratorArrayNode = previewConfig.getFieldNode(decoratorNodePath);
+    }
+
+    // @ts-expect-error
+    // There are specific types for each kind of node and only array nodes have the elements property
+    decoratorArrayNode.elements.push(decoratorNode);
+  },
+};
