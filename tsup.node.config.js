6b60629f778045b491f05d58de31a2131e723ac0
Shaun Lloyd <shaun@chromatic.com>
2023-07-11 11:46:41 -0400

    Target node16

--- tsup.node.config.js
+++ tsup.node.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig((options) => ({
-  entry: ["src/postinstall.ts"],
-  outDir: "./bin",
-  splitting: false,
-  minify: !options.watch,
-  format: ["cjs"],
-  treeshake: true,
   clean: true,
+  entry: ["src/postinstall.ts"],
+  format: ["cjs"],
+  minify: !options.watch,
+  outDir: "./bin",
   platform: "node",
+  splitting: false,
+  target: "node16",
+  treeshake: true,
   esbuildOptions(options) {
     options.conditions = ["module"];
   },
