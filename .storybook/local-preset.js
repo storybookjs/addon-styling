7e6e0085da673af771cc838966cdd0ffb8d3f4a6
Shaun Evening <shaun@chromatic.com>
2023-07-13 13:07:04 -0400

    Move to yarn 3; Add missing peer deps (#69)
    * Update yarn to v3
    * Remove unused code
    * Simplify tsup setup
    * Add missing peerDeps
    
    Co-authored-by: Norbert de Langen <ndelangen@me.com>

--- .storybook/local-preset.js
+++ .storybook/local-preset.js
@@ -2,4 +2,4 @@ export const previewAnnotations = [require.resolve("../dist/preview.mjs")];
 
 export const managerEntries = [require.resolve("../dist/manager.mjs")];
 
-export * from "../dist/preset.mjs";
+export * from "../dist/preset.js";
---
+++ tsup.config.js
@@ -0,0 +1,39 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig((options) => [
+  {
+    entry: [
+      "src/index.ts",
+      "src/preview.ts",
+      "src/manager.ts",
+      "src/preset.ts",
+    ],
+    splitting: false,
+    minify: !options.watch,
+    format: ["cjs", "esm"],
+    dts: {
+      resolve: true,
+    },
+    treeshake: true,
+    sourcemap: true,
+    clean: true,
+    platform: "browser",
+    esbuildOptions(options) {
+      options.conditions = ["module"];
+    },
+  },
+  {
+    entry: ["src/postinstall.ts"],
+    outDir: "./bin",
+    splitting: false,
+    minify: !options.watch,
+    format: ["cjs"],
+    treeshake: true,
+    target: "node16",
+    clean: true,
+    platform: "node",
+    esbuildOptions(options) {
+      options.conditions = ["module"];
+    },
+  },
+]);
