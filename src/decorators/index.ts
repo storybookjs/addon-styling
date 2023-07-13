033353ce29f819339f3fbdbb54477a249a7d7f87
Shaun Lloyd <shaun@chromatic.com>
2023-07-13 13:49:35 -0400

    Remove theme switcher code

--- src/decorators/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./class-name.strategy";
-export * from "./data-attribute.strategy";
-export * from "./provider.strategy";
-
-export * as DecoratorHelpers from "./helpers";
--- src/preset.ts
+++ src/preset.ts
@@ -1,6 +1,3 @@
 import { webpackFinal as webpack } from "./webpack/webpackFinal";
-import { viteFinal as vite } from "./vite/viteFinal";
 
 export const webpackFinal = webpack as any;
-
-export const viteFinal = vite as any;
