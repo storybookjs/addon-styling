58af2f3bb5c6f2ccbe89bbf5497cd5e00d58ed09
Shaun Lloyd <shaun@chromatic.com>
2023-07-27 14:30:00 -0400

    Create non tagged-template way to create nodes

--- src/postinstall/utils/babel.utils.ts
+++ src/postinstall/utils/babel.utils.ts
@@ -3,10 +3,13 @@ import * as recast from "recast";
 import traverse from "@babel/traverse";
 import * as t from "@babel/types";
 
-export const stringToNode = (code: TemplateStringsArray) => {
-  const result = recast.parse(code.join(""));
+export const createNode = (code: string) => {
+  const result = recast.parse(code);
   return result.program.body;
 };
+
+export const stringToNode = (code: TemplateStringsArray) =>
+  createNode(code.join(""));
 
 export const addImports = (ast: t.File, statements: t.Statement[]) => {
   let inserted = false;
