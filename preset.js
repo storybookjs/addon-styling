040cffe202a8a323172ce0f1c19a3f94002e16b6
Shaun Lloyd <shaun@chromatic.com>
2023-08-24 09:10:31 -0400

    Stop double registry

--- preset.js
+++ preset.js
@@ -1,1 +1,10 @@
-module.exports = require("./dist/preset");
+const { webpackFinal, viteFinal } = require("./dist/preset");
+const preview = require("./dist/preview");
+const manager = require("./dist/manager");
+
+module.exports = {
+  webpackFinal,
+  viteFinal,
+  previewAnnotations: [preview],
+  managerEntries: [manager],
+};
