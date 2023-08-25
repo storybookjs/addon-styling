2fee5a96e8977e9b25b1960414c694f8132f9c0a
Shaun Evening <shaun@chromatic.com>
2023-08-25 08:10:43 -0400

    Stop double registry (#88)
    Closes #80

--- src/preset.ts
+++ src/preset.ts
@@ -4,11 +4,3 @@ import { viteFinal as vite } from "./vite/viteFinal";
 export const webpackFinal = webpack as any;
 
 export const viteFinal = vite as any;
-
-export const previewAnnotations = [
-  require.resolve("@storybook/addon-styling/preview"),
-];
-
-export const managerEntries = [
-  require.resolve("@storybook/addon-styling/manager"),
-];
